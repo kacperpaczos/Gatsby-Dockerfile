20213a360fc9167c6b7ed7ed338ed4f6ed8c0168
Kacper Paczos <kacper-paczos@linux.pl>
2023-01-23 00:36:38 +0100

    Added support for ENV variables.

--- gatsby-website/docker-entrypoint.sh
+++ gatsby-website/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #RUN npm config set unsafe-perm true
-cd /app/container-test
+cd /app/${WEBSITE}
 npm install -g gatsby-cli
 npm cache clean --force
 npm install --arch=x64 --platform=linux --legacy-peer-deps
-gatsby serve
+gatsby ${NODE_ENV}
